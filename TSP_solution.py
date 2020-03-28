8ff1a106b595eb487817017702baa6413bfc8818
Bi0x <Bi0x@foxmail.com>
2020-03-29 01:10:53 +0800

    Update for Solution

--- TSP_solution.py
+++ TSP_solution.py
@@ -6,17 +6,20 @@ class TSPSolution:
         self.rho = rho                  # 信息素挥发因子
         self.q = q                      # 信息素浓度
 
-    def initPheromone(graph)
-
-    def findSolution(self, graph, colony, sales, start, limit, opt):
-        size = len(graph.nodes)
-        antColony = colony.colonyInit(size)
-
+    # 边信息素值初始化
+    def initPheromone(graph):
         for i, j in graph.edges:
             w = graph.edges[i, j]['weight']
             if w == 0:
+                w = 1e99
+            graph.edges[i, j].setdefault('pheromone', 1 / w)
+
+    def findSolution(self, graph, colony, sales, startPole, iterTimes, opt):
+        size = len(graph.nodes)
+        antColony = colony.colonyInit(size)
+        for i in range()
 
     
     def findBest(self, graph, colony, sales, start, gen_size, limit, opt):
         res = -1
-        for i in 
+        initPheromone(graph)
